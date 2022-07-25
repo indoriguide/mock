80c6fafed94a4927842bb47f2f7212d38ec00692
!ndori guide <73743991+indoriguide@users.noreply.github.com>
2022-07-25 06:07:17 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Vienna is the capital of which country?",
+    answer: "Austria",
+    options: [
+      "Germany",
+      "France",
+      "Austria",
+      "Poland"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Who is called the father of Germanunity?",
+    answer: "Helmut Kohl",
+    options: [
+      "Helmut Kohl",
+      "Garybaldi",
+      "Bismark",
+      "Hitler"
+    ]
+  },
+    {
+    numb: 3,
+    question: "AWACS warning system is eqquiped with which aircraft?",
+    answer: "IL-76",
+    options: [
+      "Mig 21",
+      "Sukoi 30",
+      "IL-76",
+      "TEJAS"
+    ]
+  },
+    {
+    numb: 4,
+    question: "The first oil well in India was dug at?",
+    answer: "Digboi",
+    options: [
+      "Naharkatiya",
+      "Digboi",
+      "Akleshwar",
+      "Mumbai High"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which of the following is the lightest gas?",
+    answer: "Hydrogen",
+    options: [
+      "Oxygen",
+      "Nitrogen",
+      "Hydrogen",
+      "Methane"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
